7ca68615926a7773bc5663f2dd1e4b6fa8da3714
ale777opp <ale777opp@yandex.ru>
2021-02-26 15:46:57 +0300

    получение http адресов первых запсей БД и проверка их на работоспособность

--- class.ip_adress_json.php
+++ class.ip_adress_json.php
@@ -35,7 +35,7 @@ require_once('grant.php');
     }
 };
 
-function searchquery($token, $dbId, $fld, $query)
+function searchQuery($token, $dbId, $fld, $query)
 {
 require('config.php');
 $query = urlencode($query);
@@ -93,6 +93,24 @@ print_r(json_last_error_msg());
 return $error;
 } //  ---END OF isJSON
 
+function getServerResponse($url) {
+    $curlInit = curl_init($url); // Инициализация cURL
+    curl_setopt($curlInit,CURLOPT_CONNECTTIMEOUT,10); // Установка параметров запроса
+    curl_setopt($curlInit,CURLOPT_HEADER,true);
+    curl_setopt($curlInit,CURLOPT_NOBODY,true);
+    curl_setopt($curlInit,CURLOPT_RETURNTRANSFER,true);
+    $response = curl_exec($curlInit); // Выполнение запроса
+    $http_code = curl_getinfo($curlInit, CURLINFO_HTTP_CODE);
+    //echo "HTTP/1.1 $http_code <br>";
+    curl_close($curlInit); // закрываем CURL
+/*
+echo '<pre>';
+print_r($response);
+echo '</pre>';
+*/
+    return $http_code;
+} //---END OF getServerResponse
+
 function writeRecord($token, $dbId, $libid,$data_string)
 {
 require('config.php');
@@ -121,6 +139,9 @@ if ($httpcode === 200) {
         }
  */
 } //  ---END OF writequery
+
+
+
 /*------ START OF RECORD DELETE
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
--- ip_adress.php
+++ ip_adress.php
@@ -1,130 +1,73 @@
 <?php
+error_reporting(E_ALL);
+require_once('class.ip_adress_json.php');
+require('config.php');
 
-error_reporting(0);
+$FLD = 'ID';
+$QUERY = '*';
 
-if(isset($_POST['search'])) {
+print_r('idb = '.$IDB.'<br>');
+print_r('fld = '.$FLD.'<br>');
+print_r('query = '.$QUERY.'<br>');
 
-print_r('POST[idb] = '.$_POST['idb'].'<br>');
-print_r('POST[fld] = '.$_POST['fld'].'<br>');
-print_r('POST[query] = '.$_POST['query'].'<br>');
-    searchOPAC($_POST['idb'], $_POST['fld'], urlencode($_POST['query']));
-}
+['code' => $httpcode, 'content' => $auth] = authOPAC();
+$Token = $auth->access_token;
+$selected_ID = [];
 
-function searchOPAC($idb, $fld, $query = ''){
-
-    if (empty($query)) {
-        return false;
-    }
-
-    $ch = curl_init();
-
-    $request = [
-        'grant_type' => 'password',
-        'client_id' => '354FE540-6100-436F-A212-7B29C4D09545',
-        'client_secret' => 'rhBQCWiIufQRooTtXcH',
-        'username' => 'PRGUS',
-        'password' => 'wsDCrf7',
-        'scope' => 'read',
-    ];
-
-    curl_setopt($ch, CURLOPT_URL, "http://192.168.1.214/api/v1/oauth2/token");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($request));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded', 'Accept: application/json'));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $auth = json_decode(curl_exec($ch));
-    $httpcode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
-
-    print_r('Код запроса авторизации = '.$httpcode.'<br>');
-
-    echo "<pre>";
-    print_r($auth);
-    echo "</pre>";
-
+if ($httpcode === 200) {// -----START OF SELECT RECORDS
+['code' => $httpcode, 'content' => $result] = searchQuery($Token, $IDB, $FLD,$QUERY);
     if ($httpcode === 200) {
-        $record = searchquery($auth->access_token, $idb, $fld, $query);
-       if ($record) {
-            foreach ($record['data'] as $list) {
-                foreach ($list as $l) {
-                    echo "$l[leader]"; //
-                    foreach ($l['fields'] as $field) {
-                       // if ($field[tag] == '200' OR $field[tag] == '300' OR $field[tag] == '856') {
-                            echo "$field[tag]$field[data] $filed[ind1] $field[ind2] "; // !BREVE
-                        if (count($field['subfields']) > 0 ) {
-                            foreach ($field['subfields'] as $f) {
-                                echo "$f[data]$f[code]"; //!BREVE
-                            }
-                        }
-                        echo "<br>";
-                    //    }
-                    }
-                }
+    $i = 0;
+    foreach ($result['data'] as $value) {
+        $selected_ID[$i] = $value['id'];
+    $i++;
+    }
+    }
+} // -----END OF SELECT RECORDS
+foreach ($selected_ID as $current_ID) {// -----START OF PARCER
+print_r($current_ID.'<br>');
+   ['code' => $httpcode, 'content' => $result] = searchRecord($Token, $IDB, $current_ID);
+if ($httpcode == 200) { // -------- START of processing the result
+
+foreach ($result['data']['attributes'] as $fields) {
+    if (is_array($fields)) {
+        foreach ($fields as $tags) {
+            if ($tags['tag'] == '200' OR $tags['tag'] == '300' OR $tags['tag'] == '856'){
+            foreach ($tags['subfields'] as $current_tag) {
+            switch ($tags['tag']) {
+                    case '200':
+                        if ($current_tag['code'] == 'a') $response_title = $current_tag['data'];
+                        break;
+                    case '300':
+                    $last_date = preg_match($FILTER_DATE,$current_tag['data']);
+
+                    if ($last_date) {$response_date = $current_tag['data'];
+                    } else {$response_date = 'Нет даты предыдущего обновления';}
+                        break;
+                    case '856':
+                        if ($current_tag['code'] == 'u') $response_http = $current_tag['data'];
+                        break;
+                    default:
+                        break;
+            }  // -----END OF SWITCH
             }
+            }
+        }
+    }
+    }
+echo "<pre> ---";
+print_r($response_title.'<br>');
+print_r($response_date.'<br>');
+print_r($response_http.'<br>');
+$source_response = getServerResponse($response_http);
+print_r($source_response);
+($source_response == '200' OR $source_response == '30*') ? print_r(" - Сайт жив.<br>") : print_r(" - Сайт умер.<br>");
+echo "---</pre>";
+}// -------- START of processing the result
+}// -----END OF PARCER
+
 /*
-           echo "<pre>";
-            print_r($record['data']);
-            echo "</pre>";
+require_once('request.php');
+writeRecord($Token, $IDB, $LIBID,$record_field);
 */
-        } else {
-            return false;
-        }
-    }
-}
-
-function searchquery($token, $dbId, $fld, $query) {
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://opac.liart.ru/api/v1/databases/" . $dbId . "/records?filter[query]=". $fld ."%20" . $query);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/vnd.api+json', 'authorization: Bearer ' . $token));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    $search = json_decode(curl_exec($ch), true);
-    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-    if ($httpcode == 200) {
-        if ( $search['meta']['count'] > 0 ) {
-            return $search;
-        } else {
-            return false;
-        }
-    }
-
-    return false;
-}
-
 ?>
-
-<!DOCTYPE html>
-<html lang="ru" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Поиск IP </title>
-        <style media="screen">
-            .search {
-                position: absolute;
-                left: 50%;
-                transform: translate(-50%, 0%);
-            }
-            .search input[type='text'] {
-                width: 800px;
-                height: 25px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="search">
-            <form method="post">
-                <select name="idb">
-                    <option selected value="425">БД Ресурсы Интернет</option>
-                </select>
-                <select name="fld">
-                    <option selected value="FT">Все поля</option>
-                <!--    <option value="ADB">Первая дата</option>
-                    <option value="ADE">Вторая дата</option> -->
-                </select>
-                <input type="text" name="query" value="">
-                <input type="submit" name="search" value="Поиск">
-            </form>
-        </div>
-    </body>
-</html>
