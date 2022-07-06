554651992dfdd0337996560a0e914b243013396b
ale777opp <ale777opp@yandex.ru>
2022-07-06 12:22:35 +0300

    начало формирования скрипта для очередной обработки 425 базы

--- regexp.php
+++
@@ -1,7 +0,0 @@
-<?php
-$string = 'http://195.178.222.75:8083/pics/Card/...6/00000154.JPGhttp://195.178.222.75:8083/pics/Card/...6/00000154.jpg';
-$pattern = '/\.jpg$/i';
-$subject ='';
-$subject = preg_match($pattern, $string, $matches);
-echo "<pre>";print_r($matches);echo "</pre>";
-?>
